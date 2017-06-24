d188cf83abfe8da53c3eaa64bacd4a1edb9358e0
Andrew Bell <fiixed@gmail.com>
2017-06-24 15:08:16 -0500

    feat:   reworked networking for FPS game

---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class mainMenu : MonoBehaviour {
+
+    public string defaultScene = "1";
+    public string defaultRoom = "Room 1";
+
+
+    public string version = "0.1 Alpha";
+
+
+    bool createRoom = false;
+
+    RoomOptions ro;
+
+
+    void Start()
+    {
+        //defaultScene = maps[0].displayName;
+        ro = new RoomOptions();
+        ro.MaxPlayers = 8;
+        if (!PhotonNetwork.connected) {
+            PhotonNetwork.ConnectUsingSettings(version);
+        }
+
+    }
+
+    public void quickPlay()
+    {
+        string newRoom = "Room1";
+        if (PhotonNetwork.GetRoomList().Length > 0)
+        {
+            joinCreate(newRoom);
+            Debug.Log(newRoom);
+            //find random room 
+            //joinCreate(PhotonNetwork.GetRoomList()[Random.Range(0, PhotonNetwork.GetRoomList().Length)].Name);
+        }
+        else
+        {
+           
+            joinCreate(newRoom);
+            Debug.Log(newRoom);
+        }
+    }
+
+
+    public void joinCreate(string nme)
+    {
+        if (PhotonNetwork.connectedAndReady)
+        {
+            PhotonNetwork.JoinOrCreateRoom(nme, ro, null);
+            //string[] roomNme = nme.Split(nameSeperator[0]);
+            //Debug.Log(roomNme[0] + "/" + roomNme[1]);
+           
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+             
+            }
+        }
+    }
+
+   
+
+   
+    
+       
+    
+    
+
